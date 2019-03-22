73413cb77cfdcdb58da9e3a0d22819c005af6d06
luyuan <1101873740@qq.com>
2019-03-22 22:44:49 +0800

    [update] 使用cdn加速

--- vue.config.js
+++ vue.config.js
@@ -4,11 +4,11 @@ const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPl
 
 const isProduction = process.env.NODE_ENV === 'production';
 const cdn = {
-  css: ['http://aonokj.cn/static/cdn/iview.css'],
-  js: ['http://aonokj.cn/static/cdn/vue.min.js',
-       'http://aonokj.cn/static/cdn/iview.min.js',
-      'http://aonokj.cn/static/cdn/vue-router.min.js',
-    'http://aonokj.cn/static/cdn/axios.min.js',]
+  css: ['/static/cdn/iview.css'],
+  js: ['/static/cdn/vue.min.js',
+       '/static/cdn/iview.min.js',
+      '/static/cdn/vue-router.min.js',
+    '/static/cdn/axios.min.js',]
 }
 
 module.exports = {
